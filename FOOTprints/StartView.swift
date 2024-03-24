bc0a781955983a251d7b17be65984788960e920c
qualbi <kevqbui6@gmail.com>
2024-03-24 11:45:40 -0400

    Menu Page

---
+++ FOOTprints/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  FOOTprints
+//
+//  Created by Kevin Bui on 3/24/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
